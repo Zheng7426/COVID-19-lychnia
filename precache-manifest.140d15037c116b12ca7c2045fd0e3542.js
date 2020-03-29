a48c86a042e644e8bedfa35e101650358d469024
Yating(Esther) Su <yaringsu@foxmail.com>
2020-03-29 19:26:50 -0400

    Updates

---
+++ precache-manifest.140d15037c116b12ca7c2045fd0e3542.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe05b8fe0d81339214277a78782e6c3",
+    "url": "/Shareforchrist/index.html"
+  },
+  {
+    "revision": "9648e262a5a61790fe95",
+    "url": "/Shareforchrist/static/css/2.7d2f2f6a.chunk.css"
+  },
+  {
+    "revision": "a47a1557b55b37442b63",
+    "url": "/Shareforchrist/static/css/main.3ffadd0a.chunk.css"
+  },
+  {
+    "revision": "9648e262a5a61790fe95",
+    "url": "/Shareforchrist/static/js/2.d3f3e1d5.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/Shareforchrist/static/js/2.d3f3e1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47a1557b55b37442b63",
+    "url": "/Shareforchrist/static/js/main.90399ea4.chunk.js"
+  },
+  {
+    "revision": "c89b9c91c2df782ef6bb",
+    "url": "/Shareforchrist/static/js/runtime-main.b3ba27ac.js"
+  },
+  {
+    "revision": "7e14c6687ff9812904124b41a467d171",
+    "url": "/Shareforchrist/static/media/个人灵修-By Eucalyp.7e14c668.png"
+  },
+  {
+    "revision": "17a364d20a2c5200f1dacc22c16e10bb",
+    "url": "/Shareforchrist/static/media/家庭育儿-By Freepik.17a364d2.png"
+  },
+  {
+    "revision": "b597abb2f9c9c4aea6bd44b4811f91bc",
+    "url": "/Shareforchrist/static/media/数据查询-By DinosoftLabs.b597abb2.png"
+  },
+  {
+    "revision": "13d6f305c2416ff894fa086f1a58501c",
+    "url": "/Shareforchrist/static/media/科普文章-By Smashicons.13d6f305.png"
+  },
+  {
+    "revision": "4a9e82faf28c216d28e3421cc157f9c8",
+    "url": "/Shareforchrist/static/media/问诊平台-By Good Ware.4a9e82fa.png"
+  },
+  {
+    "revision": "2c49edfe2950e3dda59a9a4b5cac73e0",
+    "url": "/Shareforchrist/static/media/防疫手册-By Smashicons.2c49edfe.png"
+  }
+]);
