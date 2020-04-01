0b70a5b6ce354f8ff1479753517a4185e0b53ea8
Yating(Esther) Su <yaringsu@foxmail.com>
2020-04-01 11:44:09 -0400

    Add to the router

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/COVID-19-lychnia">
       <Navbar />
       <Switch>
         <Route path="/" exact render={props => <Home />} />
